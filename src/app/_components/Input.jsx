ef89bddd7c2911d142c69b05fb9a0e1fe61a3270
b4yrra <t.chinbayr@gmail.com>
2026-01-20 11:54:43 +0800

    a

--- src/app/_components/Input.jsx
+++ src/app/_components/Input.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 
 export const Input = () => {
   const [value, setValue] = useState("");
-  const [render, setRender] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("all");
 
   const changeValue = (event) => {
     setValue(event.target.value);
@@ -11,20 +12,19 @@ export const Input = () => {
 
   const renderTasks = () => {
     if (value === "") {
-      return null;
+      return;
     } else {
-      setRender((prev) => [
-        ...prev,
-        { task: value, id: Date.now(), isCompleted: false },
-      ]);
+      const newTask = { task: value, id: Date.now(), isCompleted: false };
+
+      setTasks([...tasks, newTask]);
     }
 
     setValue("");
   };
 
   const isCheck = (id) => {
-    setRender((prev) =>
-      prev.map((task) => {
+    setTasks(
+      tasks.map((task) => {
         if (task.id === id) {
           return { ...task, isCompleted: !task.isCompleted };
         } else {
@@ -34,8 +34,25 @@ export const Input = () => {
     );
   };
 
-  const DeleteBtn = (id) => {
-    setRender((prev) => prev.filter((task) => task.id !== id));
+  const deleteBtn = (id) => {
+    setTasks((prev) => prev.filter((task) => task.id !== id));
+  };
+
+  const filterValue = (filter) => {
+    setFilter(filter);
+  };
+
+  const render = tasks.filter((task) => {
+    if (filter === "all") {
+      return task;
+    }
+    return filter === "active" ? !task.isCompleted : task.isCompleted;
+  });
+
+  const completedCount = tasks.filter((task) => task.isCompleted).length;
+
+  const clearComplete = () => {
+    setTasks((prev) => prev.filter((task) => !task.isCompleted));
   };
 
   return (
@@ -56,37 +73,94 @@ export const Input = () => {
         </button>
       </div>
       <div className="flex gap-3">
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+        <button
+          className={`p-3 text-xs cursor-pointer rounded-xl ${
+            filter === "all"
+              ? "bg-blue-600 text-white "
+              : "bg-slate-200 text-black"
+          }`}
+          onClick={() => filterValue("all")}
+        >
+          All
+        </button>
+        <button
+          className={`p-3 text-xs cursor-pointer rounded-xl ${
+            filter === "active"
+              ? "bg-blue-600 text-white "
+              : "bg-slate-200 text-black"
+          }`}
+          onClick={() => filterValue("active")}
+        >
+          Active
+        </button>
+        <button
+          className={`p-3 text-xs cursor-pointer rounded-xl ${
+            filter === "complete"
+              ? "bg-blue-600 text-white "
+              : "bg-slate-200 text-black"
+          }`}
+          onClick={() => filterValue("complete")}
+        >
+          Completed
+        </button>
       </div>
       <div className="flex flex-col gap-2">
-        {render.map((task, index) => {
-          return (
-            <div
-              key={index}
-              className="flex justify-between bg-slate-100 p-3 rounded-xl items-center h-15"
-            >
-              <div className="flex gap-2">
-                <input
-                  type="checkbox"
-                  className="w-5"
-                  checked={task.isCompleted}
-                  onChange={() => isCheck(task.id)}
-                />
+        {render.length === 0 ? (
+          <p className="text-gray-500 text-center p-10">
+            No tasks yet. Add one above!
+          </p>
+        ) : (
+          render.map((task, index) => {
+            return (
+              <div
+                key={index}
+                className="flex justify-between bg-slate-100 p-3 rounded-xl items-center h-15 text-black"
+              >
+                <div className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    className="w-5"
+                    checked={task.isCompleted}
+                    onChange={() => isCheck(task.id)}
+                  />
+                  <div>
+                    <p
+                      className={
+                        task.isCompleted ? "line-through text-gray-500" : ""
+                      }
+                    >
+                      {task.task}
+                    </p>
+                  </div>
+                </div>
+                {task.isCompleted && (
+                  <button
+                    className="text-red-500 bg-red-100 p-2 rounded-2xl cursor-pointer"
+                    onClick={() => deleteBtn(task.id)}
+                  >
+                    Delete
+                  </button>
+                )}
               </div>
-              {task.isCompleted && (
-                <button
-                  className="text-red-500 bg-red-100 p-2 rounded-2xl cursor-pointer"
-                  onClick={() => DeleteBtn(task.id)}
-                >
-                  Delete
-                </button>
-              )}
-            </div>
-          );
-        })}
+            );
+          })
+        )}
       </div>
+      {render.length === 0 ? (
+        <p></p>
+      ) : (
+        <div className="flex justify-between">
+          <div className="text-gray-500">
+            {completedCount} of {render.length} tasks completed
+          </div>
+          <button
+            className="text-red-500 cursor-pointer"
+            onClick={clearComplete}
+          >
+            Clear complete
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex justify-center mt-40">
       <div className="flex flex-col gap-5 items-center shadow-2xl w-10xs min-h-56 h-fit p-5 rounded-xl">
-        <h1 className="text-2xl font-bold">To-Do Lists</h1>
+        <h1 className="text-2xl font-bold text-black">To-Do Lists</h1>
         <Input />
       </div>
     </div>
